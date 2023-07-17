01eb2cd7d38812222baaae323ff37128de118ace
Jan Becka <janbecka@lego.com>
2023-07-17 17:10:55 +0200

    refactor healthcheck to separated handler module config basic function

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,26 +1,48 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 )
 
-func main() {
-	mux := http.NewServeMux()
-	mux.HandleFunc("/v1/healthcheck", healthcheck)
+const version = "1.0.0"
 
-	err := http.ListenAndServe(":4000", mux)
+type config struct {
+	port int
+	env  string
+}
+
+type application struct {
+	config config
+	logger *log.Logger
+}
+
+func main() {
+	var cfg config
+
+	flag.IntVar(&cfg.port, "port", 4000, "API server port")
+	flag.StringVar(&cfg.env, "env", "dev", "Environment (dev|stage|prod)")
+	flag.Parse()
+
+	logger := log.New(os.Stdout, "", log.Ldate|log.Ltime)
+
+	app := &application{
+		config: cfg,
+		logger: logger,
+	}
+
+	addr := fmt.Sprintf(":%d", cfg.port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/v1/healthcheck", app.healthcheck)
+
+	logger.Printf("starting %s server on %s", cfg.env, addr)
+
+	err := http.ListenAndServe(addr, mux)
 	if err != nil {
 		fmt.Print(err)
 	}
-}
-
-func healthcheck(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.Fprintln(w, "status: available")
-	fmt.Fprintf(w, "enviroment: %s\n", "dev")
-	fmt.Fprintf(w, "version %s\n", "1.0.0")
 }
